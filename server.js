d66b421deeaee0af821f7351572dab5034fe02e8
rudrakushwaha <rudrakshkushwaha907@gmail.com>
2026-05-05 20:05:23 +0530

    / route add

--- server.js
+++ server.js
@@ -20,6 +20,10 @@ app.use("/api/tasks", taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
